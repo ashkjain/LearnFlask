7f3ba1b88084643d83b603c461652eb34b5225da
ashishjain-repo <ashishjain99@yahoo.com>
2024-05-14 10:31:41 -0600

    Rendered Dynamic Data with nested dictionary

--- app.py
+++ app.py
@@ -2,10 +2,36 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {
+    'id':1,
+    'title' : 'Data Analyst',
+    'location':'Delhi, India',
+    'salary': 'Rs. 10,00,000'
+    },
+    {
+    'id': 2,
+    'title' : 'Data Scientist',
+    'location':'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+    },
+    {
+    'id':3,
+    'title' : 'Frontend Engineer',
+    'location':'Remote'
+    },
+    {
+    'id':4,
+    'title' : 'Backend Engineer',
+    'location':'San Jose, USA',
+    'salary': '$120,000'
+    }
+    ]
+
 @app.route("/")
 def hello_world():
-    return render_template('./home.html')
+    return render_template('./home.html', jobs=JOBS, company_name = 'Jains')
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True, port=5000)
+    app.run(debug=True, host="0.0.0.0", port=5000)
